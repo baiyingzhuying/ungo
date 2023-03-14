14619756c1984c5cbb3b19b5bb3f7c17c53bc7b1
沈海涛 <115347609+haitaoshen66@users.noreply.github.com>
2023-03-14 20:53:13 +0800

    Add files via upload

---
+++ MainWindow/item.cpp
@@ -0,0 +1,11 @@
+#include "item.h"
+
+Item::Item()
+{
+
+}
+Item::Item(QPoint pt ,bool bBlack)
+{
+    m_pt=pt;
+    m_bBlack=bBlack;
+}
---
+++ MainWindow/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include<QVector>
+#include"item.h"
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void DrawCHessBroad();//绘制棋盘
+    void DrawItems();//绘制棋子
+    QVector<Item> m_items;//记录我们的落子
+    bool m_bIsBlackTun;//记录落子颜色
+    int CountRoundItems(Item it,QPoint pt);
+    void mousePressEvent(QMouseEvent *event);
+    void paintEvent(QPaintEvent *event);
+
+
+
+private:
+    Ui::MainWindow *ui;
+protected:
+};
+#endif // MAINWINDOW_H
