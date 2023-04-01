c5d07ff8cb285f7a26420edd675a9865e972d271
沈海涛 <115347609+haitaoshen66@users.noreply.github.com>
2023-04-01 14:29:16 +0800

    Add files via upload

---
+++ OP version （dynamic）/yonghu.h
@@ -0,0 +1,50 @@
+#ifndef YONGHU_H
+#define YONGHU_H
+
+#include<qsettings.h>
+#include<QMessageBox>
+#define CONFIGPATH (QCoreA)
+#include <QWidget>
+
+extern QString White_player;
+extern QString Black_player;
+extern int havewhite;
+extern int haveblack;
+
+namespace Ui {
+class YongHu;
+}
+
+class YongHu : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit YongHu(QWidget *parent = nullptr);
+    ~YongHu();
+
+    int White_score,Black_score;
+    void login_check_black();
+    void login_check_white();
+    void zhuce_check();
+//    QString White_player;
+//    QString Black_player;
+//    int havewhite;
+//    int haveblack;
+    friend class MainWindow_01;
+
+
+
+
+private slots:
+
+signals:
+    void sendWhiteUser(QString &text);
+    void sendBlackUser(QString &text);
+
+private:
+    Ui::YongHu *ui;
+
+};
+
+#endif // YONGHU_H
